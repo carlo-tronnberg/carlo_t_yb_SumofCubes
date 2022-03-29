bf7a0c4328412c5c3e0b301b645d4519da8c5e71
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2022-03-29 16:05:37 +0200

    chore: ✨ create dummy files
    Create dummy test and implementation files

---
+++ test/sumofCubes.test.js
@@ -0,0 +1,21 @@
+const { carlo_t_yb_SumofCubes } = require('../src/sumofCubes');
+
+
+describe('carlo_t_yb_SumofCubes', () => {
+  it('Start a new sumofCubes', () => {
+    const sumofCubes = new carlo_t_yb_SumofCubes();
+
+    expect(sumofCubes.getStatus()).toEqual('running');
+  });
+});
+
+describe('carlo_t_yb_SumofCubes 2', () => {
+  it.each([
+    [1, 'one'],
+    [2, 'two'],
+  ])("Given ,      When %i,      Then  '%s'", (x, result) => {
+    const sumofCubes = new carlo_t_yb_SumofCubes();
+    expect(sumofCubes.getValue(x)).toEqual(result);
+  });
+});
+
